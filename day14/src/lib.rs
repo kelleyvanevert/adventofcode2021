7cef3b1e81f10d1afdc38186206efe7d2b004420
Kelley van Evert <hello@klve.nl>
2022-12-30 14:03:29 +0100

    Day 14 completed

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -1,12 +1,61 @@
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, iter::Sum};
 
-type Polymer = Vec<char>;
+type Polymer = Vec<(char, char)>;
 type Rules = HashMap<(char, char), char>;
+
+#[derive(Debug, Clone)]
+struct Histo(HashMap<char, u64>);
+
+impl Histo {
+    fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    fn from<const N: usize>(arr: [(char, u64); N]) -> Self {
+        Self(HashMap::from(arr))
+    }
+
+    fn add(&mut self, c: char, num: u64) {
+        self.0.entry(c).and_modify(|n| *n += num).or_insert(num);
+    }
+}
+
+impl Sum for Histo {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        let mut sum = Histo::new();
+
+        for item in iter {
+            sum = add_histos(sum, item);
+        }
+
+        sum
+    }
+}
+
+impl IntoIterator for Histo {
+    type Item = (char, u64);
+    type IntoIter = std::collections::hash_map::IntoIter<char, u64>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+fn add_histos(mut x: Histo, y: Histo) -> Histo {
+    for (c, num) in y {
+        x.add(c, num);
+    }
+
+    x
+}
 
 fn parse(s: &str) -> (Polymer, Rules) {
     let (template, rules) = s.split_once("\n\n").unwrap();
-    let template = template.chars().collect::<Vec<_>>();
+    let template = template
+        .chars()
+        .tuple_windows::<(char, char)>()
+        .collect::<Vec<_>>();
 
     let rules = rules
         .trim()
@@ -23,48 +72,77 @@ fn parse(s: &str) -> (Polymer, Rules) {
     (template, rules)
 }
 
-fn step(polymer: Polymer, rules: &Rules) -> Polymer {
-    let mut last = '?';
-
-    let mut new_polymer = polymer
-        .iter()
-        .tuple_windows()
-        .flat_map(|(&a, &b)| {
-            last = b;
-            if let Some(&c) = rules.get(&(a, b)) {
-                vec![a, c]
-            } else {
-                vec![a]
-            }
-        })
-        .collect_vec();
-
-    new_polymer.push(last);
-    new_polymer
+std::thread_local! {
+  static CALC_MEMO: RefCell<HashMap<(char,char,usize),Histo>> = RefCell::new(HashMap::new());
 }
 
-fn run(s: &str, steps: usize) -> usize {
-    let (template, rules) = parse(s);
-
-    let mut polymer = template;
-    for i in 0..steps {
-        println!("{i}");
-        polymer = step(polymer, &rules);
+fn calc(le: char, ri: char, steps: usize, rules: &Rules) -> Histo {
+    if steps == 0 {
+        return Histo::from([(le, 1)]);
+    } else if let Some(&insert) = rules.get(&(le, ri)) {
+        return add_histos(
+            calc_memoized(le, insert, steps - 1, rules),
+            calc_memoized(insert, ri, steps - 1, rules),
+        );
     }
 
-    let histo = polymer.into_iter().counts();
-    let mut histo = histo.iter().collect_vec();
+    Histo::from([(le, 1)])
+}
+
+/// Basically just copied what `#[memoize]` generates, except omitting the rules
+fn calc_memoized(le: char, ri: char, steps: usize, rules: &Rules) -> Histo {
+    let r = CALC_MEMO.with(|cell| {
+        let memo = cell.borrow_mut();
+        memo.get(&(le, ri, steps)).cloned()
+    });
+
+    if let Some(r) = r {
+        return r;
+    }
+
+    let r = calc(le, ri, steps, rules);
+
+    CALC_MEMO.with(|cell| {
+        let mut memo = cell.borrow_mut();
+        memo.insert((le, ri, steps), r.clone());
+    });
+
+    r
+}
+
+fn run(template: Polymer, rules: Rules, steps: usize) -> Histo {
+    let mut histo = template
+        .iter()
+        .map(|&(le, ri)| calc_memoized(le, ri, steps, &rules))
+        .sum::<Histo>();
+
+    let last_char = template.last().unwrap().1;
+    histo.add(last_char, 1);
+
+    histo
+}
+
+fn score(histo: Histo) -> u64 {
+    let mut histo = histo.into_iter().collect_vec();
     histo.sort_by_key(|t| t.1);
 
     histo.last().unwrap().1 - histo.first().unwrap().1
 }
 
-pub fn solve(s: &str) -> usize {
-    run(s, 10)
+pub fn solve(s: &str) -> u64 {
+    let (template, rules) = parse(s);
+
+    let histo = run(template, rules, 10);
+
+    score(histo)
 }
 
-pub fn bonus(s: &str) -> usize {
-    run(s, 40)
+pub fn bonus(s: &str) -> u64 {
+    let (template, rules) = parse(s);
+
+    let histo = run(template, rules, 40);
+
+    score(histo)
 }
 
 #[test]
@@ -90,5 +168,5 @@ CN -> C
 ";
 
     assert_eq!(solve(s), 1588);
-    // assert_eq!(bonus(s), 2188189693529);
+    assert_eq!(bonus(s), 2188189693529);
 }
